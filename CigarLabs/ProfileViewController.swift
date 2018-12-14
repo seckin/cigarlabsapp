e5b8ccbdf17c18a0b028273f7cd92354a5c0da05
Seckin Can Sahin <seckincansahin@gmail.com>
2018-12-13 19:11:33 -0500

    dislaying fullname, saving and displaying email

--- CigarLabs/ProfileViewController.swift
+++ CigarLabs/ProfileViewController.swift
@@ -14,12 +14,12 @@ class ProfileViewController: QuickTableViewController, UITextFieldDelegate {
 
         var user = PFUser.current()
         let fullName = user!["fullName"] as! String
-        let email = "email"//user!.object(forKey: "email") as! String
+        let email = user!.email as! String
 
         tableContents = [
             Section(title: "Info", rows: [
-                NavigationRow(title: "Full Name", subtitle: .rightAligned(fullName), icon: .named("time"), action: showDetail()),
-                NavigationRow(title: "Email", subtitle: .rightAligned(email), icon: .named("time"), action: { _ in }),
+                NavigationRow(title: "Full Name", subtitle: .rightAligned(fullName), icon: .named("time"), action: showFullnameDetail()),
+                NavigationRow(title: "Email", subtitle: .rightAligned(email), icon: .named("time"), action: showEmailDetail()),
                 ]),
 
             Section(title: "Settings", rows: [
@@ -39,7 +39,7 @@ class ProfileViewController: QuickTableViewController, UITextFieldDelegate {
         // ...
     }
 
-    private func showDetail() -> (Row) -> Void {
+    private func showFullnameDetail() -> (Row) -> Void {
         return { [weak self] in
             let detail = $0.title
             let controller = UIViewController()
@@ -50,6 +50,33 @@ class ProfileViewController: QuickTableViewController, UITextFieldDelegate {
 //            sampleTextField.placeholder = ""
             var user = PFUser.current()
             sampleTextField.text = user!["fullName"] as! String
+            sampleTextField.tag = 0
+            sampleTextField.font = UIFont.systemFont(ofSize: 15)
+            sampleTextField.borderStyle = UITextField.BorderStyle.roundedRect
+            sampleTextField.autocorrectionType = UITextAutocorrectionType.no
+            sampleTextField.keyboardType = UIKeyboardType.default
+            sampleTextField.returnKeyType = UIReturnKeyType.done
+            sampleTextField.clearButtonMode = UITextField.ViewMode.whileEditing
+            sampleTextField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+            sampleTextField.delegate = self
+            controller.view.addSubview(sampleTextField)
+
+            self?.navigationController?.pushViewController(controller, animated: true)
+        }
+    }
+
+    private func showEmailDetail() -> (Row) -> Void {
+        return { [weak self] in
+            let detail = $0.title
+            let controller = UIViewController()
+            controller.view.backgroundColor = .white
+            controller.title = detail
+
+            let sampleTextField =  UITextField(frame: CGRect(x: 20, y: 100, width: 300, height: 40))
+            //            sampleTextField.placeholder = ""
+            var user = PFUser.current()
+            sampleTextField.text = user!.email
+            sampleTextField.tag = 1
             sampleTextField.font = UIFont.systemFont(ofSize: 15)
             sampleTextField.borderStyle = UITextField.BorderStyle.roundedRect
             sampleTextField.autocorrectionType = UITextAutocorrectionType.no
@@ -76,55 +103,68 @@ class ProfileViewController: QuickTableViewController, UITextFieldDelegate {
         }
     }
 
-    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-        // return NO to disallow editing.
-        print("TextField should begin editing method called")
-        return true
-    }
-
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        // became first responder
-        print("TextField did begin editing method called")
-    }
-
-    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-        // return YES to allow editing to stop and to resign first responder status. NO to disallow the editing session to end
-        print("TextField should snd editing method called")
-        return true
-    }
+//    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+//        // return NO to disallow editing.
+//        print("TextField should begin editing method called")
+//        return true
+//    }
+//
+//    func textFieldDidBeginEditing(_ textField: UITextField) {
+//        // became first responder
+//        print("TextField did begin editing method called")
+//    }
+//
+//    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+//        // return YES to allow editing to stop and to resign first responder status. NO to disallow the editing session to end
+//        print("TextField should snd editing method called")
+//        return true
+//    }
 
     func textFieldDidEndEditing(_ textField: UITextField) {
         // may be called if forced even if shouldEndEditing returns NO (e.g. view removed from window) or endEditing:YES called
+
         print("TextField did end editing method called")
-        var user = PFUser.current()
-        user?["fullName"] = textField.text
+        let user = PFUser.current()
+        if textField.tag == 0 {
+            user?["fullName"] = textField.text
+        }
+        if textField.tag == 1 {
+            user?["email"] = textField.text
+        }
         user?.saveInBackground()
+        self.viewDidLoad()
     }
 
     func textFieldDidEndEditing(_ textField: UITextField, reason: UITextFieldDidEndEditingReason) {
         // if implemented, called in place of textFieldDidEndEditing:
         print("TextField did end editing with reason method called", textField.text)
-        var user = PFUser.current()
-        user?["fullName"] = textField.text
+        let user = PFUser.current()
+        if textField.tag == 0 {
+            user?["fullName"] = textField.text
+        }
+        if textField.tag == 1 {
+            user?["email"] = textField.text
+        }
         user?.saveInBackground()
+        self.viewDidLoad()
     }
 
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        // return NO to not change text
-        print("While entering the characters this method gets called")
-        return true
-    }
-
-    func textFieldShouldClear(_ textField: UITextField) -> Bool {
-        // called when clear button pressed. return NO to ignore (no notifications)
-        print("TextField should clear method called")
-        return true
-    }
-
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        // called when 'return' key pressed. return NO to ignore.
-        print("TextField should return method called")
-        // may be useful: textField.resignFirstResponder()
-        return true
-    }
+//    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//        // return NO to not change text
+//        print("While entering the characters this method gets called")
+//        return true
+//    }
+//
+//    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+//        // called when clear button pressed. return NO to ignore (no notifications)
+//        print("TextField should clear method called")
+//        return true
+//    }
+//
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        // called when 'return' key pressed. return NO to ignore.
+//        print("TextField should return method called")
+//        // may be useful: textField.resignFirstResponder()
+//        return true
+//    }
 }
